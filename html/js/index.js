a47f1d3c4ff6a25638294d942b28df19e498e213
Giovanni Bettinazzi <giovanni.bettinazzi@bottega52.it>
2016-03-17 21:18:28 +0100

    [UI] Login Loader and Modal Alert

---
+++ html/js/index.js
@@ -0,0 +1,13 @@
+var appMaster = {
+
+    preLoader: function () {
+        imageSources = []
+        $('img').each(function () {
+            var sources = $(this).attr('src');
+            imageSources.push(sources);
+        });
+        if ($(imageSources).load()) {
+            $('.pre-loader').fadeOut('slow');
+        }
+    }
+}
